abb0bd5a426fd4d6373ee8980bc1285660d46626
Mattews-Sty <mattews3007@gmail.com>
2025-05-05 16:24:45 -0500

    update: Añadiendo simplejwt en Installed_Apps

--- backend/settings.py
+++ backend/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'usuarios',
     'corsheaders',
     'django.contrib.sites',
