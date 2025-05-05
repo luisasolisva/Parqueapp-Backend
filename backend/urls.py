01bbe674403d0137fe20a0f821fb6beb11115e13
Mattews-Sty <mattews3007@gmail.com>
2025-05-05 16:18:17 -0500

    delete: urls innecesarias

--- backend/urls.py
+++ backend/urls.py
@@ -4,14 +4,8 @@ from django.http import HttpResponse
 from django.urls import path , include
 from django.contrib import admin
 
-def home(request):
-    if request.user.is_authenticated:
-        return HttpResponse("¡Bienvenido, al backend!")
-    return redirect('/accounts/login/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('usuarios.urls')),
-    path('accounts/', include('allauth.urls')),
-    path('', home),
 ]
