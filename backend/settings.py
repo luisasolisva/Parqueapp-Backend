480fb8ed558361c062532d1520a870684d80b73c
Mattews-Sty <mattews3007@gmail.com>
2025-05-06 21:25:15 -0500

    update: Installed_Apps en settings

--- backend/settings.py
+++ backend/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'django_extensions',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
