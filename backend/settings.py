68775aebbe505021c8569ee726f6f852a6525f99
Mattews-Sty <mattews3007@gmail.com>
2025-05-06 21:18:15 -0500

    update: lenguaje en español

--- backend/settings.py
+++ backend/settings.py
@@ -145,14 +145,10 @@ AUTHENTICATION_BACKENDS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'es'
 USE_I18N = True
-
-USE_TZ = True
-
+USE_L10N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
