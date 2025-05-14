73b026b0e94026e653da1dfa95c8b4a5c83082f5
luisasolisva <luisasolis930@gmail.com>
2025-05-14 18:51:28 -0500

    update: serializer y vista con tokens access

--- parqueadero/views.py
+++ parqueadero/views.py
@@ -5,8 +5,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from usuarios.models import Reserva
-from rest_framework.permissions import AllowAny
 
 
 
@@ -17,7 +15,7 @@ from .serializers import ReservaSerializer
 
 class CrearReservaView(CreateAPIView):
     serializer_class = ReservaSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated] 
 
     def perform_create(self, serializer):
         serializer.save(id_usuario=self.request.user)
