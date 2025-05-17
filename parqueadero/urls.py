3278282ca513ea461f57f8b14b831048fae582ca
luisasolisva <luisasolis930@gmail.com>
2025-05-16 21:04:19 -0500

    update:urls crear y listar

--- parqueadero/urls.py
+++ parqueadero/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import ParqueaderosCercanosView
+from .views import CrearParqueaderoView, lista_parqueaderos
 
 urlpatterns = [
     path('cercanos/', ParqueaderosCercanosView.as_view(), name='parqueaderos_cercanos'),
+    path('crear-parqueadero/', CrearParqueaderoView.as_view(), name='crear-parqueadero'),
+    path('parqueaderos/', lista_parqueaderos, name='lista_parqueaderos'),
 ]
