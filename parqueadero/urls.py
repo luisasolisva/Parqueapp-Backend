3b9070650ebb4c31858ac75b4939d7f0bbbcb4a5
luisasolisva <luisasolis930@gmail.com>
2025-05-21 22:25:45 -0500

    Update urls modifcar matriz y parque

--- parqueadero/urls.py
+++ parqueadero/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import ParqueaderosCercanosView
 from .views import CrearParqueaderoView, lista_parqueaderos
-from .views import modificar_matriz_parqueadero
-from .views import VerMatrizParqueaderoView
+from .views import ModificarParqueaderoView
+from .views import VerMatrizParqueaderoView,ModificarMatrizParqueaderoView
 from . import views
 
 
@@ -12,6 +12,8 @@ urlpatterns = [
     path('cercanos/', ParqueaderosCercanosView.as_view(), name='parqueaderos_cercanos'),
     path('crear-parqueadero/', CrearParqueaderoView.as_view(), name='crear-parqueadero'),
     path('parqueaderos/', lista_parqueaderos, name='lista_parqueaderos'),
-    path('parqueadero/<uuid:id_parqueadero>/modificar/', views.modificar_matriz_parqueadero, name='modificar-matriz'),
-    path('parqueaderos/<uuid:id_parqueadero>/ver-matriz/', VerMatrizParqueaderoView.as_view(), name='ver_matriz'),
+    path('parqueaderos/<uuid:id_parqueadero>/modificar/', ModificarMatrizParqueaderoView.as_view(), name='modificar-matriz'),
+    path('parqueaderos/<uuid:id_parqueadero>/ver-matriz/', VerMatrizParqueaderoView.as_view(), name='ver_matriz')
+    path('modificar/<uuid:id_parqueadero>/', ModificarParqueaderoView.as_view(), name='modificar_parqueadero'),
+
 ]
