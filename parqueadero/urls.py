b65d70349fc550a7a1ee568e1bdf6d3a96dae433
luisasolisva <luisasolis930@gmail.com>
2025-05-29 13:41:00 -0500

    Update:urls  de registrar,listar,ver,modificarespacios,modificar parqueadero,listaespacios y guardar espacios

--- parqueadero/urls.py
+++ parqueadero/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
-from .views import (
-    ParqueaderosCercanosView,)
+from .views import ParqueaderosCercanosView
+from .views import RegistrarParqueaderoView, lista_parqueaderos
+from .views import ModificarParqueaderoView, VerEspaciosParqueaderoView
+from .views import  ListaEspaciosDisponiblesView, GuardarEspaciosDisponiblesView, ModificarEspaciosParqueaderoView
+from . import views
+
 
 urlpatterns = [
     path('cercanos/', ParqueaderosCercanosView.as_view(), name='parqueaderos_cercanos'),
+    path('Registrar-parqueadero/', RegistrarParqueaderoView.as_view(), name='Registrar-parqueadero'),
+    path('lista/', lista_parqueaderos, name='lista_parqueaderos'),
+    path('modificar/<uuid:id_parqueadero>/', ModificarParqueaderoView.as_view(), name='modificar_parqueadero'),
+    path('Espacios-disponibles/<uuid:id_parqueadero>/', ListaEspaciosDisponiblesView.as_view(), name='lista_espacios_disponibles'),
+    path('Guardar_espacios/<uuid:id_parqueadero>/', GuardarEspaciosDisponiblesView.as_view(), name='Guardar_espacios'),
+    path('modificar_espacios/', ModificarEspaciosParqueaderoView.as_view(), name='modificar_espacios_parqueadero'),
+    path('ver_espacios/', VerEspaciosParqueaderoView.as_view(), name='ver_espacios'),
     
 ]
