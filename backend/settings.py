ff5bd9d682d81f9c4b24cdce88b8544bf5d15187
Mattews-Sty <mattews3007@gmail.com>
2025-05-30 18:02:14 -0500

    fix: Error emitido por sessionAutentication en producción

--- backend/settings.py
+++ backend/settings.py
@@ -250,7 +250,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        #Comentar en Produccion 'rest_framework.authentication.SessionAuthentication'
+        'rest_framework.authentication.SessionAuthentication', 
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
