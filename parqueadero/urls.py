48673bc0afd009a046186de3e7e1a880ffd6cc1a
luisasolisva <luisasolis930@gmail.com>
2025-06-01 18:50:29 -0500

    Update: urls espacios disponibles

--- parqueadero/urls.py
+++ parqueadero/urls.py
@@ -10,13 +10,12 @@ urlpatterns = [
     path('cercanos/', ParqueaderosCercanosView.as_view(), name='parqueaderos_cercanos'),
     path('Registrar-parqueadero/', RegistrarParqueaderoView.as_view(), name='Registrar-parqueadero'),
     path('lista/', lista_parqueaderos, name='lista_parqueaderos'),
-    path('modificar/<uuid:id_parqueadero>/', ModificarParqueaderoView.as_view(), name='modificar_parqueadero'),
-    path('Espacios-disponibles/<uuid:id_parqueadero>/', ListaEspaciosDisponiblesView.as_view(), name='lista_espacios_disponibles'),
+    path('Modificar/<uuid:id_parqueadero>/', ModificarParqueaderoView.as_view(), name='modificar_parqueadero'),
     path('Guardar_espacios/<uuid:id_parqueadero>/', GuardarEspaciosDisponiblesView.as_view(), name='Guardar_espacios'),
     path('Ver_espacios/<uuid:id_parqueadero>', VerEspaciosParqueaderoView.as_view(), name='ver_espacios'),
     path('Estadisticas/<uuid:id_parqueadero>/', EstadisticasAdminView.as_view(), name='Estadisticas'),
     path('Detalles_parqueadero/<uuid:id_parqueadero>/', ParqueaderoDetailView.as_view(), name='parqueadero_detalle'),
     path('Eliminar_parqueadero/<uuid:id_parqueadero>/', EliminarParqueaderoView.as_view(), name='Eliminar_parqueadero'),
-    path("parqueadero/Modificar_espacios/<uuid:id_parqueadero>/", ModificarEspaciosParqueaderoView.as_view(), name="modificar_espacios_parqueadero"),
-    
+    path('Modificar_espacios/<uuid:id_parqueadero>/', ModificarEspaciosParqueaderoView.as_view(), name="modificar_espacios_parqueadero"),
+    path('Ver_espacios/<uuid:id_parqueadero>/', VerEspaciosParqueaderoView.as_view(), name="ver_espacios"),
 ]
