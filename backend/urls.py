630b548296d07e284a5618be77b7e8459b844bed
Mattews-Sty <mattews3007@gmail.com>
2025-06-03 20:59:11 -0500

    update: Ajustes en la estructura de las Urls

--- backend/urls.py
+++ backend/urls.py
@@ -5,6 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('usuarios.urls')),
-    path('api/parqueadero/', include('parqueadero.urls')),
-    path('parqueaderos/', include('parqueadero.urls'))
+    path('api/parqueaderos/', include('parqueadero.urls')),
 ]
