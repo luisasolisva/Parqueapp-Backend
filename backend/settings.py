5f8f20ba73c995b9eab7c05287118bd02eaaf243
luisasolisva <luisasolis930@gmail.com>
2025-06-04 00:02:03 -0500

    add: crear app reservas

--- backend/settings.py
+++ backend/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'usuarios',
+    'reservas',
     'parqueadero',
     'corsheaders',
     'django.contrib.sites',
